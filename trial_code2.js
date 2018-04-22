7b179b99d08095b133d7f54d9e22d3f9397510b8
Ballinchanman <cbrown02@college.harvard.edu>
2018-04-22 15:29:11 -0400

    change to html and javascript

---
+++ trial_code2.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("button").click(function(){
+		$.getJSON('https://api-v3.mbta.com/routes', function(data) {
+	 		console.log("This is mbtainfo", data);
+	 		document.write(data);
+	 		document.write(data.object);
+	 		mbtainfo = data;
+	 	});
+	 });
+
+
+$("h1").click(function() {
+    document.write(mbtainfo);
+  });
+  
+});
+
+
+//https://api-v3.mbta.com/routes
+//curl -X GET "https://api-v3.mbta.com/trips?sort=block_id&filter%5Bdirection_id%5D=0" -H "accept: application/vnd.api+json"
