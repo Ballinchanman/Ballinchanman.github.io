9ac91647d328c4d36ae418bfd5f5ee7cffeb614e
Ballinchanman <cbrown02@college.harvard.edu>
2018-04-23 01:17:27 -0400

    trying to put stopinfo in array

--- trial_code2.js
+++ trial_code2.js
@@ -1,36 +1,48 @@
 // $(document).ready(function(){
 //     $("button").click(function(){
-// 		$.getJSON('https://api-v3.mbta.com/routes?filter[type]=3', function(data) {
-// 	 		console.log("This is mbtainfo", data);
-// 	 		document.write(JSON.stringify(data.data[0].attributes.color));
-// 	 		//document.write(data.object);
-// 	 		//mbtainfo = data;
-// 	 	});
-// 	 });
+//      $.getJSON('https://api-v3.mbta.com/routes?filter[type]=3', function(data) {
+//          console.log("This is mbtainfo", data);
+//          document.write(JSON.stringify(data.data[0].attributes.color));
+//          //document.write(data.object);
+//          //mbtainfo = data;
+//      });
+//   });
 // });
 
 
 
 $(document).ready(function(){
     $("button").click(function(){
-		$.getJSON('https://api-v3.mbta.com/routes?filter[type]=3', function(routedata) {
-	 		console.log("This is mbtainfo", routedata);
-	 		var routeid = routedata.data[0].id;
-	 		$.getJSON(`https://api-v3.mbta.com/schedules?filter[route]=${routeid}`, function(routeschedule) {
-	 			//document.write(JSON.stringify(routeschedule));
-	 			//document.write(JSON.stringify(routeschedule.data[0].relationships));
-	 			tripid = routeschedule.data[0].relationships.trip.data.id;
-	 			document.write(JSON.stringify(tripid));
-	 		});
-	 		// document.write(JSON.stringify(tripid));
-	 		// $.getJSON(`https://api-v3.mbta.com/schedules?filter[route]=${routeid}`, function(routeschedule) {
-	 		// 	//document.write(JSON.stringify(routeschedule));
-	 		// 	//document.write(JSON.stringify(routeschedule.data[0].relationships));
-	 		// 	scheduleid = routeschedule.data[0].relationships.trip.data.id;
-	 		// 	document.write(JSON.stringify(scheduleid));
-	 		// });
-	 	});
-	 });
+        triproutes = [];
+        $.getJSON('https://api-v3.mbta.com/routes?filter[type]=3', function(routedata) {
+            console.log("This is mbtainfo", routedata);
+            var routeid = routedata.data[0].id;
+            $.getJSON(`https://api-v3.mbta.com/schedules?filter[route]=${routeid}`, function(routeschedule) {
+                //document.write(JSON.stringify(routeschedule));
+                //document.write(JSON.stringify(routeschedule.data[0].relationships));
+                tripid = routeschedule.data[0].relationships.trip.data.id;
+                //document.write(JSON.stringify(tripid));
+                $.getJSON(`https://api-v3.mbta.com/schedules?filter[trip]=${tripid}`, function(tripschedule) {
+                    tripschedule.data.forEach(function(element, index, data) {
+                        triproutes.push(JSON.stringify(element.relationships.stop.data.id))
+                    });
+                    document.write(triproutes);
+
+                    // $.getJSON(`https://api-v3.mbta.com/stops?filter[trip]=${tripid}`, function(tripschedule) {
+
+                    // }                    
+                });
+            });
+            // document.write(JSON.stringify(tripid));
+            // $.getJSON(`https://api-v3.mbta.com/schedules?filter[route]=${routeid}`, function(routeschedule) {
+            //  //document.write(JSON.stringify(routeschedule));
+            //  //document.write(JSON.stringify(routeschedule.data[0].relationships));
+            //  scheduleid = routeschedule.data[0].relationships.trip.data.id;
+            //  document.write(JSON.stringify(scheduleid));
+            // });
+        });
+        document.write(triproutes);
+     });
 });
 
 
