d49ef965bfb72091001e4d03606f1d86417dbcf1
Ballinchanman <cbrown02@college.harvard.edu>
2018-04-23 02:59:56 -0400

    cut out some extra

--- trial_code2.js
+++ trial_code2.js
@@ -46,11 +46,25 @@ $(document).ready(function(){
 
 
 
+
+                        // for(let i = 0; i < triproutes.length; i++) {
+                        //     document.write("Stop " + tripschedule.data[i].attributes.stop_sequence + ", " 
+                        //         + tripstops.data[i].attributes.name
+                        //         + ", has coordinates " + tripstops.data[i].attributes.latitude + " and " + tripstops.data[i].attributes.longitude +
+                        //         ", headed " + "INBOUND OR OUTBOUND" + " direction. It is scheduled to arrive at " + tripschedule.data[i].attributes.arrival_time + ".<br>");
+                        // }
+
+
+                        // var tablestring = "<div><table style=" + "width:100%" + ">" +
+                        //   "<tr><th>Name</th><th>Stop Number</th> <th>Arrival Time</th><th>Longitude</th><th>Latitude</th></tr><tr>" +
+                        //     "<tr><td>" + tripstops.data[0].attributes.name + "</td><td>" + tripschedule.data[0].attributes.stop_sequence + "</td>" +
+                        //     "<td>" + tripschedule.data[0].attributes.arrival_time + "</td><td>" + tripstops.data[0].attributes.longitude + "</td>" +
+                        //     "<td>" + tripstops.data[0].attributes.latitude + "</td></tr></tr></table></div>"
                         var tablestring = "<div><table style=" + "width:100%" + ">" +
-                          "<tr><th>Name</th><th>Stop Number</th> <th>Arrival Time</th><th>Longitude</th><th>Latitude</th></tr><tr>" +
+                          "<tr><th>Name</th><th>Stop Number</th> <th>Arrival Time</th><th>Longitude</th><th>Latitude</th></tr>" +
                             "<tr><td>" + tripstops.data[0].attributes.name + "</td><td>" + tripschedule.data[0].attributes.stop_sequence + "</td>" +
                             "<td>" + tripschedule.data[0].attributes.arrival_time + "</td><td>" + tripstops.data[0].attributes.longitude + "</td>" +
-                            "<td>" + tripstops.data[0].attributes.latitude + "</td></tr></tr></table></div>"
+                            "<td>" + tripstops.data[0].attributes.latitude + "</td></tr></table></div>"
                         document.write(tablestring);
                           // <tr>
                           //   {% for dict in table %}
